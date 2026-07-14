f20f53736c3e0a16973a94bfb703181a43371f2f
Samuel Åkesson <sermuns@lysator.liu.se>
2026-07-14 19:36:40 +0200

    initial commit

---
+++ xtask/src/main.rs
@@ -0,0 +1,56 @@
+use std::{fs, path::Path, time::Instant};
+
+use itertools::Itertools;
+
+const MAIN_RS_CONTENTS: &str = r#"fn main() {
+    println!("hello world");
+}"#;
+
+fn create_crate(name: &str) -> anyhow::Result<()> {
+    let crate_dir = Path::new("crates").join(name);
+    fs::create_dir(&crate_dir)?;
+
+    let cargo_toml_contents = format!(
+        r#"[package]
+name = "{}"
+edition.workspace = true
+publish.workspace = true"#,
+        name,
+    );
+    fs::write(crate_dir.join("Cargo.toml"), cargo_toml_contents)?;
+
+    let src_dir = crate_dir.join("src");
+    fs::create_dir(&src_dir)?;
+
+    fs::write(src_dir.join("main.rs"), MAIN_RS_CONTENTS)?;
+
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    let mut number_of_crates = 0u64;
+
+    let before = Instant::now();
+
+    let _ = fs::remove_dir_all("crates");
+    fs::create_dir("crates")?;
+
+    for len in 1..=2 {
+        for name_ascii_bytes in std::iter::repeat(b'a'..b'z')
+            .take(len)
+            .multi_cartesian_product()
+        {
+            let name = str::from_utf8(&name_ascii_bytes)?;
+            create_crate(name)?;
+            number_of_crates += 1;
+        }
+    }
+
+    println!(
+        "generated {} crates in {:?}",
+        number_of_crates,
+        before.elapsed()
+    );
+
+    Ok(())
+}
